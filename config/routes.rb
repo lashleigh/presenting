1bc0530bbfc084247c747a0d7f089b16bf8edcb4
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-02-26 01:15:23 -0800

    Added failure method of auth.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Presenting::Application.routes.draw do
   post "update" => "slideshows#update", :as => "update"
 
   match "/auth/:provider/callback" => "sessions#create"  
+  match "/auth/failure" => "sessions#failure"  
+
   match "/signout" => "sessions#destroy", :as => :signout  
 
   root :to => "home#index"
